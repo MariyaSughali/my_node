62bbfff7e8ad237f6cfcd5d2a781f8c04875426a
Mariya sughali <mariyasughali@gmail.com>
2023-07-25 18:32:10 +0530

    edited mynode.js

--- mynode.js
+++ mynode.js
@@ -2,7 +2,7 @@ const express= require ("express");
 const app = express();
 
 app.get("/",(req,res)=>{
-    res.send("hello world from MY NODE")
+    res.send("hello world from MY NODE from vs to git ")
 })
 
 app.listen(3000);
